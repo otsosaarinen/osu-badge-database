9ec8bf7f92fa9c6d588abd88832b3b44593996c0
Otso Saarinen <saarinenotso@gmail.com>
2025-02-24 10:49:33 +0200

    Update database.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,6 @@
 import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database("./backend/database/badges.db", (err) => {
+const db = new sqlite3.Database("./backend/db/badges.db", (err) => {
     if (err) {
         console.error("Error connecting to database");
         console.log(err);
