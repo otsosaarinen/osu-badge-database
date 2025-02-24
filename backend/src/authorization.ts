c2fa6a98af409ed34e676e77cf91a6e413e2982b
Otso Saarinen <saarinenotso@gmail.com>
2025-02-24 11:06:35 +0200

    Create authorization.ts

---
+++ backend/src/authorization.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+const clientCredentialsURL = new URL("https://osu.ppy.sh/oauth/token");
