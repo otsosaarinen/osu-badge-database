0e46a98afaf9a021b2e68181c2618ed19424bcf6
Otso Saarinen <saarinenotso@gmail.com>
2025-02-24 14:49:49 +0200

    Update fetchRanking.js

--- backend/dist/fetchRanking.js
+++ backend/dist/fetchRanking.js
@@ -1,44 +1,73 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator["throw"](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected);
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next()
+            );
+        });
+    };
 import dotenv from "dotenv";
 dotenv.config();
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-const fetchRanking = (country, page) => __awaiter(void 0, void 0, void 0, function* () {
-    const rankingsURL = new URL("https://osu.ppy.sh/api/v2/rankings/osu/performance");
-    if (country || page) {
-        const parameters = { country, page };
-        Object.keys(parameters).forEach((key) => rankingsURL.searchParams.append(key, parameters[key]));
-    }
-    else {
-        console.error("Error with parameters");
-    }
-    try {
-        const response = yield fetch(rankingsURL, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-                Authorization: `Bearer ${ACCESS_TOKEN}`,
-            },
-        });
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+const fetchRanking = (page) =>
+    __awaiter(void 0, void 0, void 0, function* () {
+        const rankingsURL = new URL(
+            "https://osu.ppy.sh/api/v2/rankings/osu/performance"
+        );
+        if (page) {
+            const parameters = { page };
+            Object.keys(parameters).forEach((key) =>
+                rankingsURL.searchParams.append(key, parameters[key])
+            );
+        } else {
+            console.error("Error with parameters");
         }
-        const data = yield response.json();
-        return data;
-    }
-    catch (error) {
-        console.error("Error fetching ranking: ", error);
-        return null;
-    }
-});
-fetchRanking("FI", "2").then((rankingdata) => {
+        try {
+            const response = yield fetch(rankingsURL, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                    Authorization: `Bearer ${ACCESS_TOKEN}`,
+                },
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = yield response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching ranking: ", error);
+            return null;
+        }
+    });
+fetchRanking("2").then((rankingdata) => {
     console.log(rankingdata);
 });
