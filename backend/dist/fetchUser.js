955b24f9620c73bd952360512fd27bcaf89d5069
Otso Saarinen <saarinenotso@gmail.com>
2025-02-24 14:30:44 +0200

    finished fetching user data

--- backend/dist/fetchUser.js
+++ backend/dist/fetchUser.js
@@ -1,21 +1,37 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import dotenv from "dotenv";
 dotenv.config();
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-const username = "MALISZEWSKI";
-const userURL = new URL(`https://osu.ppy.sh/api/v2/users/${username}/osu`);
-const fetchUser = () => {
-    fetch(userURL, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            Authorization: `Bearer ${ACCESS_TOKEN}`,
-        },
-    })
-        .then((response) => response.json())
-        .then((data) => {
-        console.log(data);
-        console.log(data.badges.length);
-    });
-};
-fetchUser();
+const fetchUser = (username) => __awaiter(void 0, void 0, void 0, function* () {
+    const userURL = new URL(`https://osu.ppy.sh/api/v2/users/${username}/osu`);
+    try {
+        const response = yield fetch(userURL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization: `Bearer ${ACCESS_TOKEN}`,
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = yield response.json();
+        return data;
+    }
+    catch (error) {
+        console.error("Error fetching user: ", error);
+        return null;
+    }
+});
+fetchUser("MALISZEWSKI").then((userdata) => {
+    console.log(userdata);
+});
--- backend/src/fetchUser.ts
+++ backend/src/fetchUser.ts
@@ -3,24 +3,31 @@ dotenv.config();
 
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN as string;
 
-const username: string = "MALISZEWSKI";
+const fetchUser = async (username: string) => {
+    const userURL = new URL(`https://osu.ppy.sh/api/v2/users/${username}/osu`);
 
-const userURL = new URL(`https://osu.ppy.sh/api/v2/users/${username}/osu`);
-
-const fetchUser = () => {
-    fetch(userURL, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            Authorization: `Bearer ${ACCESS_TOKEN}`,
-        },
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            console.log(data.badges.length);
+    try {
+        const response = await fetch(userURL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization: `Bearer ${ACCESS_TOKEN}`,
+            },
         });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching user: ", error);
+        return null;
+    }
 };
 
-fetchUser();
+fetchUser("MALISZEWSKI").then((userdata) => {
+    console.log(userdata);
+});
