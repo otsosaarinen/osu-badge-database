97e7b31e82a5881d0d8e0bd761ed09ca42cc02ae
Otso Saarinen <saarinenotso@gmail.com>
2025-02-24 12:16:16 +0200

    Update fetchRanking.js

--- backend/dist/fetchRanking.js
+++ backend/dist/fetchRanking.js
@@ -2,10 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const rankingsURL = new URL("https://osu.ppy.sh/api/v2/rankings/osu/performance");
-const fetchRanking = (country) => {
-    if (country) {
-        const parameters = { country };
+const fetchRanking = (country, cursor) => {
+    if (country && cursor) {
+        const parameters = { country, cursor };
         Object.keys(parameters).forEach((key) => rankingsURL.searchParams.append(key, parameters[key]));
+    }
+    else {
+        console.error("Error with parameters");
     }
     fetch(rankingsURL, {
         method: "GET",
@@ -16,11 +19,14 @@ const fetchRanking = (country) => {
         },
     })
         .then((response) => response.json())
-        .then((data) => console.log(data.ranking));
+        .then((data) => {
+        console.log(data.ranking);
+        console.log(data.cursor);
+    });
     /*.then((data: Ranking) => {
             data.ranking.forEach((ranking) => {
                 console.log(ranking.user);
             });
         });*/
 };
-fetchRanking();
+fetchRanking("FI", "2");
