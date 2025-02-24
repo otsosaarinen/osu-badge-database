d05d2697e4156e0271f8f528a016c1235b266269
Otso Saarinen <saarinenotso@gmail.com>
2025-02-24 10:47:21 +0200

    Update database.ts

---
+++ backend/src/database.ts
@@ -0,0 +1,23 @@
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database("./backend/database/badges.db", (err) => {
+    if (err) {
+        console.error("Error connecting to database");
+        console.log(err);
+    } else {
+        console.log("Connected to database succesfully");
+    }
+});
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS badges (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL UNIQUE,
+        username STRING NOT NULL,
+        badges INTEGER,
+        rank INTEGER,
+        pp REAL,
+        country STRING
+        )
+    `);
+});
