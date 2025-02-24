05996a4d22a3a07378fb35a945b040f9f4e45cc3
Otso Saarinen <saarinenotso@gmail.com>
2025-02-24 11:06:33 +0200

    Update fetchRanking.ts

--- backend/src/fetchRanking.ts
+++ backend/src/fetchRanking.ts
@@ -1,5 +1,29 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const CLIENT_ID = process.env.CLIENT_ID as string;
-const CLIENT_SECRET = process.env.CLIENT_SECRET as string;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN as string;
+
+const rankingsURL = new URL(
+    "https://osu.ppy.sh/api/v2/rankings/osu/performance"
+);
+
+const fetchRanking = (country: string) => {
+    const parameters: { [key: string]: string } = { country };
+
+    Object.keys(parameters).forEach((key) =>
+        rankingsURL.searchParams.append(key, parameters[key])
+    );
+
+    fetch(rankingsURL, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+};
+
+fetchRanking("FI");
