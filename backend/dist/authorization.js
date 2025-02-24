7ead5a86b0ca8b8336bf808418d3c6a32f74d77d
Otso Saarinen <saarinenotso@gmail.com>
2025-02-24 12:16:13 +0200

    finished function for retrieving access token

--- backend/dist/authorization.js
+++ backend/dist/authorization.js
@@ -3,3 +3,15 @@ dotenv.config();
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const clientCredentialsURL = new URL("https://osu.ppy.sh/oauth/token");
+const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/x-www-form-urlencoded",
+};
+let body = `client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials&scope=public`;
+fetch(clientCredentialsURL, {
+    method: "POST",
+    headers,
+    body: body,
+})
+    .then((response) => response.json())
+    .then((data) => console.log(data));
